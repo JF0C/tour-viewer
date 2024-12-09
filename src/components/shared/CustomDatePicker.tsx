94c7981123639c534bcd07258591f44d8bc703f1
Jan Chen <jan_201@hotmail.de>
2024-12-09 20:58:06 +0100

    disable date picker reset button when date unset

--- src/components/shared/CustomDatePicker.tsx
+++ src/components/shared/CustomDatePicker.tsx
@@ -12,11 +12,11 @@ export type CustomDatePickerProps = {
 }
 
 export const CustomDatePicker: FunctionComponent<CustomDatePickerProps> = (props) => {
-
+    const disabled = props.value === undefined;
     return <div className="date-picker flex flex-row relative">
         <DatePicker sx={{ padding: 0 }} value={props.value === undefined ? undefined : dayjs(props.value)} label={props.label} format="DD.MM.YYYY" onChange={e => props.onChange?.(e?.toDate())  } />
         <div className="absolute reset-button h-full flex flex-row items-center">
-            <Button onClick={() => props.onChange?.(undefined)} sx={{ color: 'white', maxWidth: '20px' }}>
+            <Button disabled={disabled} onClick={() => props.onChange?.(undefined)} sx={{ color: 'white', maxWidth: '20px' }}>
                 <FontAwesomeIcon icon={faXmarkCircle} />
             </Button>
         </div>
